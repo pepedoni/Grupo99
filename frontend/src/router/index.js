984f37734e3789d432bf4ae27e7d502165143dda
Pedro Moutinho <phpedromoutinho@gmail.com>
2023-10-30 22:53:04 -0300

    feat: create login view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: '/login',
+        name: 'Login',
+        component: () => import('@/views/Login.vue'),
+      }
     ],
   },
 ]
